0a2b50610316cc869a78b42c4519cbf458471e00
James Pierce <jamespierce@fastmail.com>
2019-12-09 21:10:46 -0600

    dev allows all http methods cors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'localhost:3001'
-      resource '*', headers: :any, methods: [:get, :post, :options]
+      resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch]
     end
   end
 
